dfe42f6f467321814971640ee2f4ffb39976fbb9
eeefafeff3 <7091818@gmail.com>
2025-03-10 15:51:30 +0800

    Update and rename 12355.sh to 15666.sh

--- 15666.sh
+++ 15666.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 直接设置HTTP代理端口为 
-CUSTOM_PORT=12355
+CUSTOM_PORT=15666
 
 # 更新系统包列表
 #sudo apt-get update -y
@@ -19,7 +19,7 @@ http_port $CUSTOM_PORT
 http_access allow all
 
 # 强制使用127.0.0.53作为DNS服务器
-dns_nameservers 43.207.140.5
+dns_nameservers 54.168.166.67
 
 # 默认配置保持不变
 cache_dir ufs /var/spool/squid 100 16 256
