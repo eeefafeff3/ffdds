d9f2bdb4322584e06d09a04d65616a9e8600e12e
eeefafeff3 <7091818@gmail.com>
2025-03-12 13:27:26 +0800

    Update 8455.sh

--- 8255.sh
+++ 8255.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# 直接设置HTTP代理端口为8255
-CUSTOM_PORT=8255
+# 直接设置HTTP代理端口为
+CUSTOM_PORT=8455
 
 # 更新系统包列表
 #sudo apt-get update -y
@@ -19,7 +19,7 @@ http_port $CUSTOM_PORT
 http_access allow all
 
 # 强制使用8.26.56.26作为DNS服务器
-dns_nameservers 8.26.56.26
+dns_nameservers 107.20.126.164
 
 # 默认配置保持不变
 cache_dir ufs /var/spool/squid 100 16 256
