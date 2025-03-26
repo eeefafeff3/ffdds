f176b9130a2571713e66a754858165f7a25903f2
eeefafeff3 <7091818@gmail.com>
2025-03-26 20:30:30 +0800

    Update and rename wg6692.sh to wg66912.sh

--- wg66912.sh
+++ wg66912.sh
@@ -208,13 +208,10 @@ ALLOWED_IPS="0.0.0.0/0,::/0"
 # 输出设置的 Allowed IPs
 echo "Using Allowed IPs: ${ALLOWED_IPS}"
 
-
-echo ""
-echo "Okay, that was all I needed. We are ready to setup your WireGuard server now."
-echo "You will be able to generate a client at the end of the installation."
-
-# 自动继续执行，不需要用户按键
-sleep 3  # 可选：添加1秒延迟，模拟一些时间间隔
+	echo ""
+	echo "Okay, that was all I needed. We are ready to setup your WireGuard server now."
+	echo "You will be able to generate a client at the end of the installation."
+	read -n1 -r -p "Press any key to continue..."
 
 
 function installWireGuard() {
